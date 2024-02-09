7829318f6ad9360dffbaf9f8c7ed97ef7fe834b3
abbi964 <abhinavthapliyal964@gmail.com>
2024-02-09 09:28:56 +0530

    Adding Restaurant Summary

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,23 +1,40 @@
 import React from "react";
-import './Header.css';
+import "./Header.css";
+import Card from "../UI/Card";
 
-function Header(){
-
-    return(
-        <header className="header">
-            <nav className="nav_bar">
-                <h1>ReactMeals</h1>
-                <div className="header_cart">
-                    <img className="cart_img" alt="cart-icon" src="/media/cart-icon.png"></img>
-                    <h4 className="your_cart">Your Cart</h4>
-                    <h4 className="cart_item_count">0</h4>
-                </div>
-            </nav>
-            <div className="restaurant_banner">
-                <img src="/media/restaurant_banner.jpg" alt="restaurant_banner"></img>
-            </div>
-        </header>
-    )
+function Header() {
+  return (
+    <header className="header">
+      <Card className="header_card">
+        <nav className="nav_bar">
+          <h1>ReactMeals</h1>
+          <div className="header_cart">
+            <img
+              className="cart_img"
+              alt="cart-icon"
+              src="/media/cart-icon.png"
+            ></img>
+            <h4 className="your_cart">Your Cart</h4>
+            <h4 className="cart_item_count">0</h4>
+          </div>
+        </nav>
+        <div className="restaurant_banner">
+          <img src="/media/restaurant_banner.jpg" alt="restaurant_banner"></img>
+        </div>
+      </Card>
+      <Card className="summary">
+        <h1>Delicious Food, Delivered To You</h1>
+        <p>
+          Choose your favorite mean from our broad selection of avalable meals
+          or enjoy a delicious lunch or dinner at home
+        </p>
+        <p>
+          All our meals are cooked with high quality ingredients, just in time
+          and ofcourse by experienced chefs!
+        </p>
+      </Card>
+    </header>
+  );
 }
 
 export default Header;
